9b7762140e9959fe6c16432a3611852dc03ca768
howard suarez <howardesuarez@gmail.com>
2020-07-27 19:21:21 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
 def introduction(name)
-    puts "hi,my name is {name}."
+    puts "hi,my name is #{name}."
   end
+  
+  def introduction (name,language)
+    puts "hi, my name is #{name} and i am learning to program in #{langauge}."
